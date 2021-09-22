330afe95e15b88119f123d3b7dac131f0929d4a8
lugasalhawariy <lugasalhawariy@gmail.com>
2021-09-23 03:30:33 +0700

    lugas:progress page details

--- script/navbar-scroll.js
+++ script/navbar-scroll.js
@@ -1,6 +1,7 @@
 $(function() {
     $(document).scroll(function() {
         var $header = $(".navbar-fixed-top");
-        $header.toggleClass("scrolled", $(this).scrollTop() > $header.height() + 330);
+        var $page = $(".page-content");
+        $header.toggleClass("scrolled", $(this).scrollTop() > $header.height() + 330 || $(this).scrollTop() > $page.height() + 200);
     });
 });
